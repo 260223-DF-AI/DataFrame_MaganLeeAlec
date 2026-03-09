6c8b9b814202090666b28caee3f71cec7e8dc920
Alec Figueroa <afigueroa17@neiu.edu>
2026-03-09 00:11:40 -0500

    Added test case for strings as index

--- CafeSalesAnalysis/src/sales_analysis/clean_data.py
+++ CafeSalesAnalysis/src/sales_analysis/clean_data.py
@@ -1,17 +1,24 @@
 import pandas as pd
 
 
-def set_index(data: pd.DataFrame | dict, column_index: pd.Series | list | pd.Index) -> pd.DataFrame:
+def change_index(data: pd.DataFrame | dict, column_index: pd.Series | list | pd.Index) -> pd.DataFrame:
     """Returns data frame with updated index of column that uniquely identifies
     each record.""" 
 
+    # Raise exceptions if data types are not supported
     if not isinstance(data, (pd.DataFrame, dict)):
         raise TypeError("data must be a Data Frame or dictionary")
     
-    if not isinstance(column_index, (pd.Series, list, pd.Index)):
+    if not isinstance(column_index, (pd.Series, list, pd.Index, str)):
         raise TypeError("index must be a Pandas Series object or list")
     
-    pd_index = pd.Index(column_index)
+    # if index is a string, turn it to series then into pandas index. else just turn it directly to pandas index
+    if type(column_index) == str:
+        pd_index = pd.Index(data[column_index])
+    else:
+        pd_index = pd.Index(column_index)
+        
+    #if data is already a data frame, plug into set index function - else covert it to a data frame first
     if isinstance(data, pd.DataFrame):
         data.set_index(pd_index, inplace=True)
         return data
--- CafeSalesAnalysis/tests/test_clean_data.py
+++ CafeSalesAnalysis/tests/test_clean_data.py
@@ -11,13 +11,16 @@ def __init__(self):
     [
         ({"Name": ["Jack", "Jane", "Jake"]}, [0, 1, 2], pd.Index([0, 1, 2])),
         ({"Name": ["Meg", "Marge", "Mike"],
-          "Email": ["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"]}, ["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"], pd.Index(["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"]))
+          "Email": ["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"]}, ["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"], pd.Index(["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"])),
+        ({"Name": ["Meg", "Marge", "Mike"],
+          "Email": ["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"]}, "Email", pd.Index(["meg123@email.net", "marge764@wahoo.com", "mikeaw@usa.gov"]))
     ])
-def test_set_index(data: dict | pd.DataFrame, index: list | pd.Series | pd.Index, expected_index: list | pd.Series | pd.Index):
+def test_change_index(data: dict | pd.DataFrame, index: list | pd.Series | pd.Index, expected_index: list | pd.Series | pd.Index):
     """Tests if the index being set will change accurately"""
-    actual = clean_data.set_index(data, index)
+    actual = clean_data.change_index(data, index)
     
     # Cannot use == here for Pandas Index object type.
-    # Use Pandas assert for index for accurate assertions instead of pytest
-    pd.testing.assert_index_equal(actual.index, expected_index, check_order=True)
+    # Use Pandas assert on index for accurate assertions instead of pytest. includes data type comparisons and order check
+    # assert actual.index.equals(expected_index)
+    pd.testing.assert_index_equal(actual.index, expected_index, check_order=False)
     
