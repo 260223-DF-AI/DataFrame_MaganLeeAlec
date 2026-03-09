a21dc3d9d5ef2e8e7193a4014c8bf607958b140b
Alec Figueroa <aleclfig@yahoo.com>
2026-03-09 15:48:28 -0500

    added change_data_type and round_floats method

--- CafeSalesAnalysis/src/sales_analysis/clean_data.py
+++ CafeSalesAnalysis/src/sales_analysis/clean_data.py
@@ -1,14 +1,14 @@
 import pandas as pd
+import datetime
 
 
 def change_index(data: pd.DataFrame | dict, column_index: pd.Series | list | pd.Index) -> pd.DataFrame:
-    """Returns data frame with updated index of column that uniquely identifies
+    """Returns data frame with updated index of column_str that uniquely identifies
     each record.""" 
 
     # Raise exceptions if data types are not supported
     if not isinstance(data, (pd.DataFrame, dict)):
         raise TypeError("data must be a Data Frame or dictionary")
-    
     if not isinstance(column_index, (pd.Series, list, pd.Index, str)):
         raise TypeError("index must be a Pandas Series object or list")
     
@@ -27,7 +27,39 @@ def change_index(data: pd.DataFrame | dict, column_index: pd.Series | list | pd.
         return pd.DataFrame(data, index=pd_index)
     
     
+def change_data_type(data: pd.DataFrame | dict, column_str: str, type_change: type) -> pd.DataFrame:
+    """Changes the data type of a column_str in the data frame"""
 
- 
-
+    # Raise exceptions if data types aren't supported
+    if not isinstance(column_str, str):
+        raise TypeError("Column must be a string")
+    if not isinstance(data, (pd.DataFrame, dict)):
+        raise TypeError("data must be a Pandas Data Frame or dictionary")
+    if not isinstance(type_change, (type, datetime)):
+        raise TypeError("Type to change to must be a python primitive type or DateTime object")
     
+    data_frame = None
+    if isinstance(data, dict):
+        data_frame = pd.DataFrame(data)
+        data_frame[column_str] = data_frame[column_str].astype(type_change)
+        return data_frame
+    else:
+        data[column_str] = data[column_str].astype(type_change)
+        return data
+    
+def round_floats(data: pd.DataFrame, column_str: str, round_formatter: str) -> pd.DataFrame:
+    """Rounds the floats in the specified column to the number of places in round_formatter"""
+
+    if not isinstance(round_formatter, int):
+        raise TypeError("formatter must be an int")
+    if not isinstance(column_str, str):
+        raise TypeError("Column must be a string")
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError("data must be a data frame")
+
+    for index, field in enumerate(data[column_str]):
+        if not isinstance(field, float):
+            continue
+        else:
+            data.loc[index, field] = round(data.loc[index, field]), round_formatter
+
--- CafeSalesAnalysis/tests/test_clean_data.py
+++ CafeSalesAnalysis/tests/test_clean_data.py
@@ -4,9 +4,6 @@ import pandas as pd
 import pytest
 
 
-def __init__(self):
-    pass
-
 @pytest.mark.parametrize("data, index, expected_index",
     [
         ({"Name": ["Jack", "Jane", "Jake"]}, [0, 1, 2], pd.Index([0, 1, 2])),
@@ -17,10 +14,23 @@ def __init__(self):
     ])
 def test_change_index(data: dict | pd.DataFrame, index: list | pd.Series | pd.Index, expected_index: list | pd.Series | pd.Index):
     """Tests if the index being set will change accurately"""
+    #with pytest.raises(TypeError):
     actual = clean_data.change_index(data, index)
     
     # Cannot use == here for Pandas Index object type.
     # Use Pandas assert on index for accurate assertions instead of pytest. includes data type comparisons and order check
     # assert actual.index.equals(expected_index)
     pd.testing.assert_index_equal(actual.index, expected_index, check_order=False)
-    
+
+@pytest.mark.parametrize(
+        "data, column_str, data_type, expected_column, expected_assert", [({"Money": ["21.23", "100.57", "30.00"]}, "Money", float, {"Money": [21.23, 100.57, 30.00]}, True),
+        ({"Money": ["21.23", "NaN", "30.00"]}, "Money", float, {"Money": [21.23, float("NaN"), 30.00]}, False)]
+)
+def test_change_data_type(data, column_str, data_type, expected_column, expected_assert):
+    """Tests if a column in a dataframe will change to the declared data type"""
+    #with pytest.raises(TypeError):
+    actual = clean_data.change_data_type(data, column_str, data_type)
+
+    # asserting with pandas will raise an assert error instead of checking if it matches the expected assert
+    #pd.testing.assert_series_equal( actual[column_str], pd.DataFrame(expected_column)[column_str]) is expected_assert
+    assert actual[column_str].equals(pd.DataFrame(expected_column)[column_str]) #is expected_assert
