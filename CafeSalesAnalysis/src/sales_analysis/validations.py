dacb0d71d34a0d633815b545becf70dd8bd62260
leew735 <amelia735@revature.net>
2026-03-11 11:19:43 -0500

    added ability to read json & a json version of the dataset

--- CafeSalesAnalysis/src/sales_analysis/validations.py
+++ CafeSalesAnalysis/src/sales_analysis/validations.py
@@ -1,7 +1,6 @@
-# In here for now but can be moved - lee
 import pandas as pd
 
-
+# In here for now but can be moved - lee
 def remove_duplicate_entries(data: pd.DataFrame) -> pd.DataFrame:
     """Returns dataframe with duplicate entries removed."""
     return data.drop_duplicates()
