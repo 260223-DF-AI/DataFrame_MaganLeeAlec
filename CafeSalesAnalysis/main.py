a2e8bc602492c4e1cf765e78bfa26b0d48905d26
leew735 <amelia735@revature.net>
2026-03-12 16:26:16 -0500

    demo progress

---
+++ CafeSalesAnalysis/main.py
@@ -0,0 +1,6 @@
+from src.sales_analysis import file_reader, validation, clean_data, database, report_writer
+import pandas as pd
+
+df = file_reader.read_csv_full("dirty_cafe_sales.csv")
+list_from_schema = validation.create_schema(df, user_input=False)
+validated = validation.validate_data(df, list_from_schema)
