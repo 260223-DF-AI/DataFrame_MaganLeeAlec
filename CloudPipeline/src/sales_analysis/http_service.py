3ed96e0ce86efdcd8c22772adfec2295bf6de707
leew735 <amelia735@revature.net>
2026-03-24 11:21:22 -0500

    http_service: added clean/validate functionality

--- CloudPipeline/src/sales_analysis/http_service.py
+++ CloudPipeline/src/sales_analysis/http_service.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Body
 import json
-from src.sales_analysis import file_reader, logger
+import glob
+from src.sales_analysis import file_reader, logger,  validation
 
 app = FastAPI()
 logger = logger.setup_logger(__name__, "debug", console=False)
 
 # Send an HTTP `POST` request to trigger `.csv` to `.parquet` conversion pipelines.
-@app.post("/csv_parquet")
+@app.post("/")
 def csv_to_parquet():
     logger.debug("HTTP request recieved. Attempting to convert csv to parquet...")
 
@@ -26,22 +27,53 @@ def csv_to_parquet():
         logger.debug("Successfully read all sales batch files")
     
     # TODO: clean/validate
+    logger.debug("Validating and cleaning data...")
+    df1_valid, df2_valid, df3_valid, df4_valid, df5_valid = 0, 0, 0, 0, 0
+    try:
+        # clean all the sales data
+        df1_tuple = validation.clean_sales_data(df1)
+        df2_tuple = validation.clean_sales_data(df2)
+        df3_tuple = validation.clean_sales_data(df3)
+        df4_tuple = validation.clean_sales_data(df4)
+        df5_tuple = validation.clean_sales_data(df5)
+
+        # separate valid/rejects, validate them
+        df1_valid, df1_rejects = validation.validate_chunk_dtypes(df1_tuple[0]), 0#validation.validate_chunk_dtypes(df1_tuple[1])
+        df2_valid, df2_rejects = validation.validate_chunk_dtypes(df2_tuple[0]), 0#validation.validate_chunk_dtypes(df2_tuple[1])
+        df3_valid, df3_rejects = validation.validate_chunk_dtypes(df3_tuple[0]), 0#validation.validate_chunk_dtypes(df3_tuple[1])
+        df4_valid, df4_rejects = validation.validate_chunk_dtypes(df4_tuple[0]), 0#validation.validate_chunk_dtypes(df4_tuple[1])
+        df5_valid, df5_rejects = validation.validate_chunk_dtypes(df5_tuple[0]), 0#validation.validate_chunk_dtypes(df5_tuple[1])
+    except Exception as e:
+        logger.error(e)
+    else:
+        logger.debug("Successfully validated data")
 
     # write all batch files as parquet files
     # for now, as seperate files. if needed can be changed to write as one file
     logger.debug("Attempting to write parquet files")
     try: 
-        file_reader.write_parquet(df1, "dummy_sales_batch_1")
-        file_reader.write_parquet(df2, "dummy_sales_batch_2")
-        file_reader.write_parquet(df3, "dummy_sales_batch_3")
-        file_reader.write_parquet(df4, "dummy_sales_batch_4")
-        file_reader.write_parquet(df5, "dummy_sales_batch_5")
+        # write valid
+        file_reader.write_parquet(df1_valid, "dummy_sales_batch_1")
+        file_reader.write_parquet(df2_valid, "dummy_sales_batch_2")
+        file_reader.write_parquet(df3_valid, "dummy_sales_batch_3")
+        file_reader.write_parquet(df4_valid, "dummy_sales_batch_4")
+        file_reader.write_parquet(df5_valid, "dummy_sales_batch_5")
+
+        # write invalid
+        # file_reader.write_parquet(df1_rejects, "reject_sales_batch_1")
+        # file_reader.write_parquet(df2_rejects, "reject_sales_batch_2")
+        # file_reader.write_parquet(df3_rejects, "reject_sales_batch_3")
+        # file_reader.write_parquet(df4_rejects, "reject_sales_batch_4")
+        # file_reader.write_parquet(df5_rejects, "reject_sales_batch_5")
     except FileExistsError as e:
         logger.error(f"File already exists: {e}")
     except Exception as e:
         logger.error(e)
     else:
         logger.debug("Successfully wrote all batches as parquet files")
+    
+    logger.debug("Testing parquet write...")
+    logger.debug(f"{file_reader.read_parquet_full("dummy_sales_batch_1.parquet").tail(5)}")
 
     #TODO: trigger upload of files to GCS
     
